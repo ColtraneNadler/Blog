f6cc4ad69d79ba313ed6b8577edeb3b5f29d163a
Horrorpenguin <horrorpenguin123@gmail.com>
2015-06-01 09:47:05 -0400

    Using controller more fluently. Separating code into controller.

--- app.js
+++ app.js
@@ -17,83 +17,31 @@ var notImplemented = function(req, res) {
 };
 
 
-//ID Generator
-function randomString(length) {
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
-
-    if (! length) {
-        length = Math.floor(Math.random() * chars.length);
-    }
-
-    var str = '';
-    for (var i = 0; i < length; i++) {
-        str += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return str;
-}
-
-
 //database init
 var client = require('mongodb').MongoClient;
 
-client.connect('mongodb://admin:admin@ds031952.mongolab.com:31952/blog', function(err, db) {
-	if(err) return console.log(err);
-	console.log('Connected')
-
-	var col = db.collection('posts');
-
-	app.get('/articles', function(req, res) {
-		var articles = [];
-
-		col.find().toArray(function(err, blogPosts) {
-			if(err) return console.log(err);
-			articles = blogPosts;
-			console.log(articles);
-		
-			res.locals = {posts: articles};
-			res.render('pages/articles.ejs');
-		})
-
+(function() { client.connect('mongodb://admin:admin@ds031952.mongolab.com:31952/blog', function(err, db) {
+		if(err) return console.log(err);
+		console.log('Connected to MongoDB.');
+		global.col = db.collection('posts');
 	});
+}());
+
+	app.get('/articles', articles.list);
 
 	app.get('/', function(req, res) {
 		res.redirect('/articles')
 	})
 
 
-	app.post('/articles', function(req, res) {
-		var blogPost = req.body;
-		console.log(blogPost);
-		blogPost.timestamp = Date.now();
-		blogPost.id = randomString(6);
-
-		col.insert(blogPost, function(err, docs) {
-			if(err) return console.log(err);
-			console.log('Posted: ' + docs);
-		})
-
-		res.redirect('/articles')
-
-	});
+	app.post('/articles', articles.create);
 
 	app.get('/articles/new', function(req, res) {
 		res.render('pages/new.ejs');
 	});
 
 
-	app.get('/articles/:articleID', function(req, res) {
-		var ID = req.params.articleID;
-		col.findOne({id: ID}, function(err, doc) {
-			if(err) return console.log;
-			if(doc) {
-				console.log(doc)
-				res.locals = {post: doc};
-				res.render('pages/article.ejs');
-			} else {
-				res.redirect('/articles');
-			}
-		})
-	});
+	app.get('/articles/:articleID', articles.id);
 
 	app.put('/articles/:articleID', notImplemented);
 	app.delete('/articles/:articleID', notImplemented);
@@ -102,7 +50,6 @@ client.connect('mongodb://admin:admin@ds031952.mongolab.com:31952/blog', functio
 		res.redirect('/articles');
 	})
 
-})
 
 
 
--- controllers/articles.js
+++ controllers/articles.js
@@ -1,20 +1,70 @@
-var articles = [];;
+var articles = [];
+
+
+//ID Generator
+function randomString(length) {
+    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
+
+    if (! length) {
+        length = Math.floor(Math.random() * chars.length);
+    }
+
+    var str = '';
+    for (var i = 0; i < length; i++) {
+        str += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return str;
+}
+
+
+var client = require('mongodb').MongoClient;
+
+module.exports.connect = function() { client.connect('mongodb://admin:admin@ds031952.mongolab.com:31952/blog', function(err, db) {
+		if(err) return console.log(err);
+		console.log('Connected to MongoDB.');
+		global.col = db.collection('posts');
+	});
+};
 
 module.exports.create = function(req, res) {
 	var blogPost = req.body;
+	console.log(blogPost);
 	blogPost.timestamp = Date.now();
-	col.insert(blogPost, function(err, doc) {
+	blogPost.id = randomString(6);
+
+	col.insert(blogPost, function(err, docs) {
 		if(err) return console.log(err);
-		console.log('Posted: ' + blogPost)
+		console.log('Posted: ' + docs);
 	})
+
 	res.redirect('/articles')
+
 };
 
 
 module.exports.list = function(req, res) {
-	col.find().toArray(function(err, posts) {
+
+	col.find().toArray(function(err, blogPosts) {
 		if(err) return console.log(err);
-		articles = posts;
+		articles = blogPosts;
+		
+		res.locals = {posts: blogPosts};
+		res.render('pages/articles.ejs');
 	})
-	res.render('articles.ejs', {posts: articles});
+
+};
+
+
+module.exports.id = function(req, res) {
+	var ID = req.params.articleID;
+	col.findOne({id: ID}, function(err, doc) {
+		if(err) return console.log;
+		if(doc) {
+			console.log(doc)
+			res.locals = {post: doc};
+			res.render('pages/article.ejs');
+		} else {
+			res.redirect('/articles');
+		}
+	})
 };
